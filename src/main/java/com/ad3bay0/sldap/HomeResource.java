d725992f93e10b3a66f649c1e5a0e522fca21a11
ad3bay0 <byoextreme@hotmail.com>
2020-04-10 02:07:58 +0100

    feature: ldap spring security config

---
+++ src/main/java/com/ad3bay0/sldap/HomeResource.java
@@ -0,0 +1,15 @@
+package com.ad3bay0.sldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+
+        return "welcome home";
+    }
+
+}
